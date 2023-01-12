842eadf01d76fa2d6c34ea5c22db08fc346da4cd
pelpadesa <geoauteli2@gmail.com>
2023-01-12 03:10:36 -0500

    Add inverted color/"dark mode" images.

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 import datetime
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageOps
 from GPUs import LoadGPUs
 import tqdm
 
@@ -8,15 +8,27 @@ gpus = LoadGPUs()
 priceFont = ImageFont.truetype("segoeui.ttf", 18)
 titleFont = ImageFont.truetype("segoeuib.ttf", 48)
 
-def GenerateGraphs(region: str, currency: str):
+def _invertImage(image):
+    if image.mode != "RGBA":
+        return ImageOps.invert(image)
+    r,g,b,a = image.split()
+    rgbImage = Image.merge("RGB", (r,g,b))
+        
+    invertedImage = ImageOps.invert(rgbImage)
+    r,g,b = invertedImage.split()
+    finalImage = Image.merge("RGBA", (r,g,b,a))
+    return finalImage
+    
+
+def GenerateGraphs(region: str, currency: str, darkMode: bool = False):
     fhdImage = Image.open('./bin/1080.png')
-    fhdImage_Draw = ImageDraw.Draw(fhdImage)
-
     qhdImage = Image.open('./bin/1440.png')
-    qhdImage_Draw = ImageDraw.Draw(qhdImage)
-
     fourKImage = Image.open('./bin/4K.png')
+
+    fhdImage_Draw = ImageDraw.Draw(fhdImage)
+    qhdImage_Draw = ImageDraw.Draw(qhdImage)
     fourKImage_Draw = ImageDraw.Draw(fourKImage)
+
     for gpu in tqdm.tqdm(gpus, desc=f"{region} eBay Prices", position=0, leave=True):
         gpu.GrabListings(region=region)
         region = region.upper()
@@ -37,20 +49,29 @@ def GenerateGraphs(region: str, currency: str):
 
     fourKImage_Draw.text((870, 250), f"{region} Ebay Pricing", fill=(255, 0, 0), font=titleFont)
     fourKImage_Draw.text((1015, 305), currentDateStr, fill=(255, 0, 0), font=priceFont)
+    fourKImage_Draw.text((110, 970), "No affiliation with TomsHardware (just using their performance graphs)", fill=(255, 0, 0), font=priceFont)
 
     fhdImage_Draw.text((870, 250), f"{region} Ebay Pricing", fill=(255, 0, 0), font=titleFont)
     fhdImage_Draw.text((1015, 305), currentDateStr, fill=(255, 0, 0), font=priceFont)
-    
+    fhdImage_Draw.text((110, 970), "No affiliation with TomsHardware (just using their performance graphs)", fill=(255, 0, 0), font=priceFont)
+
     qhdImage_Draw.text((872, 250), f"{region} Ebay Pricing", fill=(255, 0, 0), font=titleFont)
     qhdImage_Draw.text((1017, 305), currentDateStr, fill=(255, 0, 0), font=priceFont)
-    
+    qhdImage_Draw.text((110, 970), "No affiliation with TomsHardware (just using their performance graphs)", fill=(255, 0, 0), font=priceFont)
+
+
+    if darkMode:
+        fhdImage = _invertImage(fhdImage)
+        qhdImage = _invertImage(qhdImage)
+        fourKImage = _invertImage(fourKImage)
+
     fhdImage.save(f'./{region}_1080.png')
     qhdImage.save(f'./{region}_1440.png')
     fourKImage.save(f'./{region}_4K.png')
 
 
 if __name__ == "__main__":
-    GenerateGraphs("USA", "$") # Region name from ./bin/Regions.json entries
+    GenerateGraphs("USA", "$", darkMode=True) # Region name from ./bin/Regions.json entries
     GenerateGraphs("UK", "£") # Currency symbol is used only for the graphs, this can be whatever you want.
     GenerateGraphs("CA", "C$")
     GenerateGraphs("AU", "A$")
