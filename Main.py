5a359bfecfc9ec7d371b01ba51b3db6c764cb18a
pelpadesa <geoauteli2@gmail.com>
2023-01-12 02:45:54 -0500

    Add tqdm progress

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import datetime
 from PIL import Image, ImageDraw, ImageFont
 from GPUs import LoadGPUs
+import tqdm
 
 gpus = LoadGPUs()
 
@@ -16,25 +17,21 @@ def GenerateGraphs(region: str, currency: str):
 
     fourKImage = Image.open('./bin/4K.png')
     fourKImage_Draw = ImageDraw.Draw(fourKImage)
-    for gpu in gpus:
+    for gpu in tqdm.tqdm(gpus, desc=f"{region} eBay Prices", position=0, leave=True):
         gpu.GrabListings(region=region)
         region = region.upper()
         
         if gpu.Coordinates[0] == "" or gpu.Coordinates[0] is None:
-            print(f"{gpu.ModelName} | Average Price: {currency}{gpu.GetAveragePrice()}")
             continue
         fhdImage_Draw.text((int(gpu.Coordinates[0].split(",")[0]), int(gpu.Coordinates[0].split(",")[1])), f"{currency}{round(gpu.GetAveragePrice())}", fill=(255, 0, 0), font=priceFont)
         
         if gpu.Coordinates[1] == "" or gpu.Coordinates[1] is None:
-            print(f"{gpu.ModelName} | Average Price: {currency}{gpu.GetAveragePrice()}")
             continue
         qhdImage_Draw.text((int(gpu.Coordinates[1].split(",")[0]), int(gpu.Coordinates[1].split(",")[1])), f"{currency}{round(gpu.GetAveragePrice())}", fill=(255, 0, 0), font=priceFont)
         
         if gpu.Coordinates[2] == "" or gpu.Coordinates[2] is None:
-            print(f"{gpu.ModelName} | Average Price: {currency}{gpu.GetAveragePrice()}")
             continue
         fourKImage_Draw.text((int(gpu.Coordinates[2].split(",")[0]), int(gpu.Coordinates[2].split(",")[1])), f"{currency}{round(gpu.GetAveragePrice())}", fill=(255, 0, 0), font=priceFont)
-        print(f"{gpu.ModelName} | Average Price: {currency}{gpu.GetAveragePrice()}")
     now = datetime.datetime.now()
     currentDateStr = now.strftime(f"%B %d, %Y")
 
