0e7c4d091a83d4a1fe87e2ec007ccb2835f78b6e
pelpadesa <geoauteli2@gmail.com>
2023-02-02 10:12:31 -0500

    Coordinate output testing
    Added _testCoordinates() to Main.py for testing the addition of new GPUs and coordinates to GPUs.json without having to re-grab all the data and plot every other GPU.

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import datetime
 from PIL import Image, ImageDraw, ImageFont, ImageOps
-from GPUs import LoadGPUs
+from GPUs import LoadGPUs, WriteData_CSV, GPU
 import tqdm
 
 gpus = LoadGPUs()
@@ -19,6 +19,56 @@ def _invertImage(image):
     finalImage = Image.merge("RGBA", (r,g,b,a))
     return finalImage
     
+def _testCoordinates(gpus: list, gpuName: str):
+    fhdImage = Image.open('./bin/1080.png')
+    qhdImage = Image.open('./bin/1440.png')
+    fourKImage = Image.open('./bin/4K.png')
+
+    fhdImage_Draw = ImageDraw.Draw(fhdImage)
+    qhdImage_Draw = ImageDraw.Draw(qhdImage)
+    fourKImage_Draw = ImageDraw.Draw(fourKImage)
+
+    for gpu in gpus:
+        if gpu.ModelName == gpuName:
+            break
+
+    if gpu.Coordinates[0] != "" and gpu.Coordinates[0] is not None:
+        fhdImage_Draw.text(
+            (int(gpu.Coordinates[0].split(",")[0]), int(gpu.Coordinates[0].split(",")[1])),
+            f"$899", fill=(255, 0, 0), font=priceFont
+        )
+        
+    if gpu.Coordinates[1] != "" and gpu.Coordinates[1] is not None:
+        qhdImage_Draw.text(
+            (int(gpu.Coordinates[1].split(",")[0]), int(gpu.Coordinates[1].split(",")[1])),
+            f"$899", fill=(255, 0, 0), font=priceFont
+        )
+        
+    if gpu.Coordinates[2] != "" and gpu.Coordinates[2] is not None:
+        fourKImage_Draw.text(
+            (int(gpu.Coordinates[2].split(",")[0]), int(gpu.Coordinates[2].split(",")[1])),
+            f"$899", fill=(255, 0, 0), font=priceFont
+        )
+
+    now = datetime.datetime.now()
+    currentDateStr = now.strftime(f"%B %d, %Y")
+
+    fourKImage_Draw.text((870, 250), f"Test Pricing", fill=(255, 0, 0), font=titleFont)
+    fourKImage_Draw.text((1015, 305), currentDateStr, fill=(255, 0, 0), font=priceFont)
+    fourKImage_Draw.text((110, 970), "No affiliation with TomsHardware (just using their performance graphs)", fill=(255, 0, 0), font=priceFont)
+
+    fhdImage_Draw.text((870, 250), f"Test Pricing", fill=(255, 0, 0), font=titleFont)
+    fhdImage_Draw.text((1015, 305), currentDateStr, fill=(255, 0, 0), font=priceFont)
+    fhdImage_Draw.text((110, 970), "No affiliation with TomsHardware (just using their performance graphs)", fill=(255, 0, 0), font=priceFont)
+
+    qhdImage_Draw.text((872, 250), f"Test Pricing", fill=(255, 0, 0), font=titleFont)
+    qhdImage_Draw.text((1017, 305), currentDateStr, fill=(255, 0, 0), font=priceFont)
+    qhdImage_Draw.text((110, 970), "No affiliation with TomsHardware (just using their performance graphs)", fill=(255, 0, 0), font=priceFont)
+
+    fhdImage.save(f'./Test_1080.png')
+    qhdImage.save(f'./Test_1440.png')
+    fourKImage.save(f'./Test_4K.png')
+
 
 def GenerateGraphs(region: str, currency: str, darkMode: bool = False):
     fhdImage = Image.open('./bin/1080.png')
